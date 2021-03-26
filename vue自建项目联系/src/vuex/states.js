cd3cea7ea9cf03233234d9e6daacce15bb246f53
jamkash <2087881719@qq.com>
2021-03-26 15:26:24 +0800

    states.js

---
+++ vue自建项目联系/src/vuex/states.js
@@ -0,0 +1,17 @@
+export default {
+  showSideNav:true,
+  tabsNum: 0,
+  parentArr:[],
+  sonArr:[],
+  grandSon:[],
+  str:'',  //由方法给值
+  IndexArr:[],  //获取那个home下显示的所有组件的索引
+  HomeIndex:[], //获取当前是那个home作为布局模板的索引
+  tabStr:0,  //tabsNum给值
+  navstr:'',
+  _this:[],
+  activeNum:'1',
+  activeNum1:'1',
+  activeNum2:'1',
+  activeNum3:'1'
+}
