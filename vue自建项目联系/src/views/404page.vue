8c2809a475b45af72a52d6a3862a38414e2f3fc1
jamkash <2087881719@qq.com>
2021-03-26 15:21:41 +0800

    404page.vue

---
+++ vue自建项目联系/src/views/404page.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="errpage">页面跑丢了/(ㄒoㄒ)/~~</div>
+</template>
+
+<script>
+    export default {
+        name: "404page"
+    }
+</script>
+
+<style scoped>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  .errpage{
+    width: 370px;
+    border: 0px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    margin: auto;
+  }
+</style>
